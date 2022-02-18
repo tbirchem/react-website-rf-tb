34053bf5df0bb4360b760f845530242bce2bd233
taylor birchem <tbirchem@westmont.edu>
2022-02-18 10:37:57 -0800

    Questionnaire*

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ function Navbar() {
           </li>
           <li className = 'nav-item'>
             <Link to='/form' className='nav-links'>
-            Questionar
+            Questionnaire
             </Link>
           </li>
         </ul>
