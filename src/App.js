73a04c4c689c47437ae054436255fba54e7a4105
taylor birchem <tbirchem@westmont.edu>
2022-03-21 13:19:18 -0700

    Defining home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route }
 from 'react-router-dom';
 import './App.css';
+import './Home.js';
 
 function App() {
   return (
