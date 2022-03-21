39cc205e964b5b577cbcc3f94728257a2041a3fd
taylor birchem <tbirchem@westmont.edu>
2022-03-21 13:38:45 -0700

    housekeeping

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route }
-from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 
 
 function App() {
   return (
-  <>
-  <Router>
-    <Navbar/>
-      <Switch>
-        <Route path='/' exact component={Home} />
-      </Switch>
-    </Router>
-    </>
+  <div>
+    <Router>
+      <Navbar/>
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
