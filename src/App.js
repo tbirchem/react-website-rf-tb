624f608734ad8d870e44f7b317ded97dbd387391
taylor birchem <tbirchem@westmont.edu>
2022-03-21 13:27:52 -0700

    Home error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route }
 from 'react-router-dom';
 import './App.css';
-import Home from './components/pages/Home';
+import Home from './components/Home';
 
 
 function App() {
