cdcb1f7e1475a7b763c95a982c0a4fdd2d0db59a
taylor birchem <tbirchem@westmont.edu>
2022-03-21 18:53:00 -0700

    Front Page up and stylized

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 
-function CardItem() {
+function CardItem(props) {
   return (
     <div>
-      <li className="cards_item">
-        <Link className="cards_item_link">
-         <figure className="cards_item_pic-wrap">
-          <img src='/' alt='Travel Image'
-          className='cards_item_img' />
+      <li className="cards__item">
+        <Link className="cards__item__link" to={props.path}>
+         <figure className="cards__item__pic-wrap" data-category={props.label}>
+          <img
+          src={props.src}
+          alt='Travel Image'
+          className='cards__item__img'
+          />
           </figure>
-          <div className='cards_item_info'>
-            <h5 className='cards_item_text'/>
+          <div className='cards__item__info'>
+            <h5 className='cards__item__text'>{props.text}</h5>
           </div>
         </Link>
       </li>
     </div>
-  )
+  );
 }
 
-export default CardItem
+export default CardItem;
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,15 +1,37 @@
-import React from 'react'
-/* import { Link } from 'react-router-dom'*/
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CardItem from './CardItem';
+import './Cards.css';
 
 function Cards() {
   return (
     <div className="cards">
-      <h1>  </h1>
-      <div className="cards_container">
-        <div className="cards_wrapper">
-          <ul className="cards_items" >
-
+      <h1> Explore </h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items" >
+            <CardItem
+            src="images/DiamondCut.jpg"
+            text="What cut is right for you?"
+            label="cut"
+            path="/cut"/>
+            <CardItem
+            src="images/DiamondColor.jpg"
+            text="What does color mean when it comes to a diamond?"
+            label="color"
+            path="/color"/>
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+            src="images/DiamondClarity.jpg"
+            text="How much does diamond clarity matter?"
+            label="clarity"
+            path="/clarity"/>
+            <CardItem
+            src="images/Questionnaire.jpg"
+            text="Fill out a questionnaire to see what ring suits you best!"
+            label="questionnaire"
+            path="/questionnaire"/>
           </ul>
         </div>
       </div>
