f132f618cffbe70048823cca3c75253b56216a41
taylor birchem <tbirchem@westmont.edu>
2022-03-21 19:02:01 -0700

    fixed alt prop to have empty string to run on netlify

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -10,7 +10,7 @@ function CardItem(props) {
          <figure className="cards__item__pic-wrap" data-category={props.label}>
           <img
           src={props.src}
-          alt='Travel Image'
+          alt=''
           className='cards__item__img'
           />
           </figure>
